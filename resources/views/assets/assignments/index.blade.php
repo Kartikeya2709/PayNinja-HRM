08875eee0d4c60b002c328643753ae5b4a58b5a0
Kartikeya_Sharma <113769547+Kartikeya2709@users.noreply.github.com>
2026-01-12 16:46:04 +0530

    asset assignment blade fix

--- resources/views/assets/assignments/index.blade.php
+++ resources/views/assets/assignments/index.blade.php
@@ -136,7 +136,7 @@ use Illuminate\Support\Facades\Crypt;
 
 <!-- in layouts.app -->
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 
 @endpush
